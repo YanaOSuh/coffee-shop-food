b740f96fe2b81f63a7229500d19671db13e5fa85
“YanaOSuh” <yanochkaiv@yahoo.com>
2024-05-25 12:31:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
+import menuBG from './menu-bg.jpg';
 
 function App() {
+
+const [dish, setDish] = useState(0);
+const {dishName, dishCompound, price, image} = data[dish];
+
+const previousDish = () => {
+  setDish((dish => {
+    dish --;
+    if (dish < 0) {
+      return data.length -1;
+    }
+    return dish;
+  }))
+}
+
+const nextDish = () => {
+  setDish((dish => {
+    dish ++;
+    if (dish > data.length -1) {
+      dish = 0;
+    }
+    return dish;
+  }))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <section className='foodSection'>
+
+    <div className='foodSection_image'>
+      <img src={menuBG} alt='cup of coffee layout'/>
     </div>
+
+    <div className='foodSection__header'>
+      <div className='foodSection__headerContainer'>
+        <h2>Order delicious food to go</h2>
+        <p>Skip the line</p>
+      </div>
+    </div>
+
+    <div className='foodContainer'>
+      <div className='foodContainer_dishPic'>
+        <img src={image} alt='dish' width='300px' />
+      </div>
+      <div className='container_dishInfo'>
+        <h3>{dishName}</h3>
+        <p className='container_dishInfo-compound'>{dishCompound}</p>
+        <p className='container_dishInfo-price'>{price}</p>
+        <button className='container_dishInfo-btn'>+ Add</button>
+      </div>
+    </div>
+
+      <div className='container_btn'>
+        <button onClick={previousDish} className='sliderBtns'>Previous</button>
+        <button onClick={nextDish} className='sliderBtns'>Next</button>
+      </div>
+    </section>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,46 @@
+import sandwich from './sandwich.jpg';
+import bananaPancakes from './bananaPancakes.jpg';
+import porridge from './porridge.jpg';
+import avocadoToast from './avocadoToast.jpg';
+import strawberryPancakes from './strawberryPancakes.jpg';
+import waffle from './waffle.jpg';
+
+
+export const data = [
+    {
+        dishName: 'Breakfast sandwich',
+        dishCompound: 'Sourdough bread, avocado, spinach, egg, cheese, pesto, red pepper flakes',
+        price: '9.99 $',
+        image: sandwich,
+    },
+    {
+        dishName: 'Banana pancakes',
+        dishCompound: 'Eggs, bananas, oats, baking powder, vanilla, milk, cinnamon, salt, honey',
+        price: '8.99 $',
+        image: bananaPancakes,
+    },
+    {
+        dishName: 'Berry porridge',
+        dishCompound: 'Porridge oats, almond milk, ground cinnamon, maple syrup, pecan nuts, strawberries, blueberries',
+        price: '7.99 $',
+        image: porridge,
+    },
+    {
+        dishName: 'Avocado toast',
+        dishCompound: '2 slices rustic sour dough bread, avocado, sea salt and pepper, crushed red pepper flakes',
+        price: '9.99 $',
+        image: avocadoToast,
+    },
+    {
+        dishName: 'Strawberry pancakes',
+        dishCompound: 'Eggs, strawberries and cherries, oats, baking powder, vanilla, milk, applesauce, honey',
+        price: '9.90 $',
+        image: strawberryPancakes,
+    },
+    {
+        dishName: 'Berry waffle',
+        dishCompound: 'Belgium waffle topped with seasonal berries, dusted with powder sugar',
+        price: '7.99 $',
+        image: waffle,
+    }
+]
